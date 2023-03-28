169b4c108839e5f5787e603eb90f3740fdb4ce7d
Haf <haf@protonmail.ch>
2023-03-28 18:19:39 +0200

    removed todo

--- src/test/kotlin/xyz/haff/testcontainers/MongoTest.kt
+++ src/test/kotlin/xyz/haff/testcontainers/MongoTest.kt
@@ -3,10 +3,8 @@ package xyz.haff.testcontainers
 import io.kotest.core.spec.style.FunSpec
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.testcontainers.junit.jupiter.Testcontainers
 import xyz.haff.testcontainers.annotation.MongoContainerTest
 
-// TODO: is the testcontainers annotation necessary?
 @DataMongoTest
 @MongoContainerTest
 class MongoTest(
